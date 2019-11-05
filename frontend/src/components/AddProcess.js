ed7e04db5439ec766f5ac5f28f0a8f4dea3808d3
ThuThaoDo91619 <dothao19121998@gmail.com>
2019-11-05 19:30:16 +0700

    create addrecipescreen

---
+++ frontend/src/components/AddProcess.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import {
+//     Image,
+    Text,
+    View,
+    StyleSheet,
+    ScrollView,
+//     TouchableOpacity,
+} from 'react-native';
+import  {Table, TableWrapper, Row, Cell, Rows} from 'react-native-table-component';
+import theme from "../../constant/theme";
+import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+export default function AddProcess(){
+    return(
+        <View style = {styles.container}>
+            <View>
+                <Text style = {styles.title}>
+                    Quy trình thực hiện
+                </Text>
+
+            </View>
+            <View>
+
+            </View>
+        </View>
+          
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor: '#fff',
+        height: theme.SIZES.BASE *15,
+        // margin: 5,
+        padding: 15,
+    },
+    title: {
+        textAlign: 'center',
+        textTransform: "uppercase",
+        marginBottom: 5,
+        fontWeight: "700",
+        marginLeft: 15,
+        marginRight: 15,
+        alignContent: "center",
+        color: "#7f0000",
+    },
+
+});
---
+++ frontend/src/components/Comment.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import {
+    Image,
+    Text,
+    View,
+    StyleSheet,
+    TouchableOpacity,
+} from 'react-native';
+import {Block,Icon} from 'galio-framework';
+import Im from "../../assets/Image/suonxaochuangot.jpg"
+import { red } from 'ansi-colors';
+import LikeImage from "../../assets/Image/Interact/like.png";
+import CommentImage from "../../assets/Image/Interact/comment.png";
+import SaveImage from "../../assets/Image/Interact/save.png";
+import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+
+export default function Comment(props){
+return(
+    <View style={styles.container}>
+        <View style = {styles.viewTitle}>
+            <Text style = {styles.title}>
+                Bình luận
+            </Text>
+            
+        </View>
+        <View style = {styles.content}>
+            <View>
+                <image>
+
+                </image>
+                <Text>
+
+                </Text>
+            </View>
+            <View>
+                <Text>
+
+                </Text>
+            </View>
+        </View>
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        flexDirection: "column",
+        alignItems: "center",
+        height: 172,
+        padding: 0,
+        backgroundColor: '#fff',
+        marginBottom: 10,
+    },
+
+    viewContent:{
+        flex: 2,
+        flexDirection: "row",
+        alignItems: "baseline",
+        padding: 5,
+    },
+
+    viewTitle:{
+        flex: 1,
+        flexDirection: 'column',
+        maxHeight: 50,
+        width: ('100%'),
+        marginTop: 0,
+        alignSelf: 'center',
+        backgroundColor: '#cdb7b5',
+    },
+
+    image:{
+        flex: 1,
+        flexDirection: "row",
+        width: 64,
+        height: 64,
+        alignItems: "center",
+        alignSelf: "center",
+    },
+
+    title:{
+        flex: 1,
+        textTransform: "uppercase",
+        marginBottom: 5,
+        fontWeight: "700",
+        marginLeft: 15,
+        marginRight: 15,
+        flexDirection: 'column',
+        alignItems: 'center',
+        alignContent: "center",
+        color: "#7f0000",
+    },
+
+    description:{
+        flex: 2,
+        marginBottom: 8,
+        paddingBottom: 0,
+        fontWeight: "300",
+        marginLeft: 15,
+    },
+
+    button:{
+        flex: 1,
+        // backgroundColor: "#b71c1c",
+        margin: 5,
+        paddingTop: 10
+        //borderRadius: 4,
+        // borderLeftWidth: 0.5,
+        // borderLeftColor: 'red'
+        
+    },
+    buttonBlock:{
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center'
+    },
+    logoButton: {
+        width: 25,
+        height:25
+    },
+});
